91a04ca647c7d179a1b6622a5fc5cab1f9475fe5
Avela337 <AvelaGxotiwe@gmail.com>
2026-05-27 08:47:13 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+let score =100
+let passingGrade = 60
+
+if(score >= passingGrade){
+    console.log("You passed!");
+} else {
+    console.log("You failed.");
+}
+
+if(score === 100){
+    console.log("Perfect score!");
+}
